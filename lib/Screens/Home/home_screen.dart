7f8b46a0161a6d2a8b09eb1025f4c596c099ccdc
RoySujon <goutomroy770@gmail.com>
2024-03-01 02:11:35 +0600

    task 2 done (tab bar done)

--- lib/Screens/Home/home_screen.dart
+++ lib/Screens/Home/home_screen.dart
@@ -1,83 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:task/Screens/Home/widgets/custome_tab_bar.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int select = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // body: SafeArea(child: Text('data')),
-      // bottomSheet: Padding(
-      //   padding: const EdgeInsets.all(30.0),
-      //   child: ,
-      // ),
-      body: SafeArea(
-        child: Card(
-          child: Column(
-            children: [
-              ...List.generate(4, (index) {
-                return Padding(
-                  padding:
-                      select == index ? EdgeInsets.all(8.0) : EdgeInsets.all(2),
-                  child: GestureDetector(
-                    onTap: () {
-                      select = index;
-                      print(select);
-                      setState(() {});
-                    },
-                    child: Container(
-                        height: 60,
-                        width: 100,
-                        decoration: BoxDecoration(
-                          borderRadius: select == index
-                              ? BorderRadius.only(
-                                  topRight: Radius.circular(50),
-                                  bottomLeft: Radius.circular(50),
-                                )
-                              : select == index + 1
-                                  ? BorderRadius.only(
-                                      bottomLeft: Radius.circular(50),
-                                      // bottomRight: Radius.circular(10),
-                                    )
-                                  : select == index - 1
-                                      ? BorderRadius.only(
-                                          // topLeft: Radius.circular(10),
-                                          topRight: Radius.circular(50))
-                                      : null,
-                          color: select == index ? Colors.red : Colors.black,
-                        ),
-                        child: Padding(
-                            padding: EdgeInsets.all(10.0),
-                            child: Center(
-                              child: Icon(
-                                iconList[index],
-                                color: Colors.white,
-                              ),
-                            )
-                            // Text(
-                            //   textList[index],
-                            //   style: TextStyle(
-                            //       color: Colors.white,
-                            //       fontWeight: FontWeight.bold,
-                            //       fontSize: 18),
-                            // ),
-                            )),
-                  ),
-                );
-              })
-            ],
-          ),
-        ),
+      appBar: AppBar(
+        title: Text('Home Screen'),
       ),
+      body: CustomeTabBar(),
     );
   }
 }
-
-List<String> textList = ["Settings", "Home", "Phone", "Chorom"];
-List<IconData> iconList = [Icons.abc, Icons.settings, Icons.home, Icons.shop];
---
+++ lib/Screens/Home/widgets/custome_tab_bar.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+
+class CustomeTabBar extends StatefulWidget {
+  const CustomeTabBar({super.key});
+
+  @override
+  State<CustomeTabBar> createState() => _CustomeTabBarState();
+}
+
+class _CustomeTabBarState extends State<CustomeTabBar> {
+  int select = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Container(
+          height: 10,
+          width: 100,
+          color: Colors.red,
+        ),
+        ...List.generate(TabBarData.tabList.length, (index) {
+          return GestureDetector(
+            onTap:
+
+                //  index != TabBarData.tabList.length - 1
+                //     ?
+
+                () {
+              select = index;
+
+              setState(() {});
+            },
+            // : null,
+            child: Container(
+                margin: select == index
+                    ? const EdgeInsets.symmetric(vertical: 15)
+                    : EdgeInsets.zero,
+                height: select == index ? 80 : 100,
+                width: select == index ? 80 : 100,
+                decoration: BoxDecoration(
+                  borderRadius: select == index
+                      ? BorderRadiusDirectional.circular(20)
+                      : select == index + 1
+                          ? const BorderRadius.only(
+                              bottomRight: Radius.circular(30),
+                              // bottomRight: Radius.circular(10),
+                            )
+                          : select == index - 1
+                              ? const BorderRadius.only(
+
+                                  // topLeft: Radius.circular(10),
+                                  topRight: Radius.circular(30))
+                              : null,
+                  color: select == index ? Colors.red : Colors.white,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      TabBarData.tabList[index].icon,
+                      color: select == index ? Colors.white : Colors.black,
+                    ),
+                    select == index
+                        ? const SizedBox()
+                        : Text(
+                            TabBarData.tabList[index].title,
+                            style: const TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 16),
+                          ),
+                  ],
+                )),
+          );
+        }),
+        Expanded(
+            child: Container(
+          constraints: const BoxConstraints(minHeight: 100),
+          decoration: BoxDecoration(
+            borderRadius: select == TabBarData.tabList.length - 1
+                ? const BorderRadius.only(topRight: Radius.circular(30))
+                : null,
+            color: Colors.white,
+          ),
+          width: 100,
+        )),
+        GestureDetector(
+          onTap: () {
+            // select = -2;
+            // setState(() {});
+          },
+          child: Container(
+            width: 100,
+            height: 100,
+            color: Colors.white,
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //  TabBarData.tabList[index].child,
+                Center(
+                  child: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        'https://a.storyblok.com/f/191576/1200x800/faa88c639f/round_profil_picture_before_.webp'),
+                  ),
+                ),
+                Text(
+                  "Profile",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Container(
+          height: 10,
+          width: 100,
+          color: Colors.red,
+        ),
+      ],
+    );
+  }
+}
+
+class TabBarData {
+  final String title;
+  final IconData icon;
+  final Widget? child;
+  TabBarData({required this.title, required this.icon, this.child});
+
+  static List<TabBarData> tabList = [
+    TabBarData(title: 'Home', icon: Icons.home),
+    TabBarData(title: 'Settings', icon: Icons.settings),
+    // TabBarData(title: 'Home', icon: Icons.home),
+    // TabBarData(title: 'Settings', icon: Icons.settings),
+    TabBarData(title: 'Phone', icon: Icons.phone),
+    TabBarData(title: 'Shop', icon: Icons.shop),
+  ];
+}
